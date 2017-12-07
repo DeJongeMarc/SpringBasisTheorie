8d4b85d11e8ad9b1ee41d96a1dea24a86d335691
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-12-07 08:57:37 +0100

    SPRING EXPRESSION LANGUAGE (SPEL)

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,17 +1,13 @@
 package be.vdab;
 
-import java.math.BigDecimal;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import be.vdab.restclients.RestClientsConfig;
-import be.vdab.services.EuroService;
-import be.vdab.services.ServicesConfig;
+import be.vdab.gaming.Spelletje;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(new Class[] {ServicesConfig.class, RestClientsConfig.class})) {
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("container.xml")) {
 			// ClassA objectA = context.getBean(ClassA.class);
 			// System.out.println(objectA.getKoersenURL());
 			// System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
@@ -28,7 +24,8 @@ public class Main {
 //			Teller teller2 = context.getBean("teller2", Teller.class);
 //			teller2.verhoog();
 //			context.getBean("teller2", Teller.class).verhoog();
-	  	System.out.println(context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2))); 
+//	    	System.out.println(context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2))); 
+			System.out.println(context.getBean(Spelletje.class).getAantalVijanden()); 
 		}
 	}
 }
